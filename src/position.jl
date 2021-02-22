892aa2b264c2e24f012fac61cc3cd6ce4c409de0
Dilschat <dilschat@yandex.ru>
2021-02-22 15:41:47 +0300

    alias for range (position) added

---
+++ src/position.jl
@@ -0,0 +1,7 @@
+Position = Union{Nothing, UnitRange{Int}}
+
+after_last(position::Position) = last(position) + 1
+before_first(position::Position) = first(position) - 1
+between(start::Position, final::Position) = after_last(start):before_first(final)
+
+Base.merge(start::Position, final::Position) = first(start):last(final)
