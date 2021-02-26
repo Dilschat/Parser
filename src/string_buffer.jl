35eeee7b97bb2d98bc2e9b28f7a40f4801b4eb16
Dilschat <dilschat@yandex.ru>
2021-02-26 22:41:07 +0300

    string buffer polished

--- src/string_buffer.jl
+++ src/string_buffer.jl
@@ -2,12 +2,14 @@ using DataStructures
 using StringViews
 const RESIZE_COEF = 1.5
 
+"""
+    Simple string buffer, that supports only 1byte chars of UTF8
+"""
 mutable struct StringBuffer
     value::Vector{UInt8}
     size::UInt32
 end
 
-#TODO add abstract type using simple traits
 StringBuffer(size::Int) = StringBuffer(Vector{UInt8}(undef, size), 0)
 
 function StringBuffer(input::String)
@@ -17,23 +19,39 @@ function StringBuffer(input::String)
 end
 
 function Base.getindex(buffer::StringBuffer, i::Integer)
-    !_in_bounds(buffer, i) && throw(BoundsError("Bound error size: $size idx: $i"))
+    !_in_bounds(buffer, i) &&
+        throw(BoundsError("Bound error size: $size idx: $i"))
     Char(buffer.value[i])
 end
+
 function Base.getindex(buffer::StringBuffer, range::UnitRange{Int64})
-    !_in_bounds(buffer, range) && throw(BoundsError("Defined range is incorrect range: $range"))
+    !_in_bounds(buffer, range) &&
+        throw(BoundsError("Defined range is incorrect range: $range"))
     StringView(@view buffer.value[range])
 end
+
 function Base.setindex!(buffer::StringBuffer, char::Char, i::Integer)
-    !_in_bounds(buffer, i) &&  throw(BoundsError("Bound error size: $size idx: $i"))
+    !_in_bounds(buffer, i) &&
+        throw(BoundsError("Bound error size: $size idx: $i"))
     buffer.value[i] = char
 end
 
-#TODO setindex?
-function Base.setindex!(buffer::StringBuffer, str::String, range::UnitRange{Int64})
-    !_in_bounds(buffer, range) && throw(BoundsError("Defined range is incorrect range: $range"))
-    ncodeunits(str) != length(range) && throw(BoundsError("Incompatable sizes str: $str range: $range"))
-    unsafe_copyto!(buffer.value, first(range), unsafe_wrap(Vector{UInt8}, str), 1, length(range))
+function Base.setindex!(
+    buffer::StringBuffer,
+    str::String,
+    range::UnitRange{Int64},
+)
+    !_in_bounds(buffer, range) &&
+        throw(BoundsError("Defined range is incorrect range: $range"))
+    ncodeunits(str) != length(range) &&
+        throw(BoundsError("Incompatable sizes str: $str range: $range"))
+    unsafe_copyto!(
+        buffer.value,
+        first(range),
+        unsafe_wrap(Vector{UInt8}, str),
+        1,
+        length(range),
+    )
 end
 
 Base.findprev(buffer::StringBuffer, val::String, startidx::Int64) = begin
@@ -46,7 +64,7 @@ Base.findprev(buffer::StringBuffer, val::String, startidx::Int64) = begin
         endidx = endidx - offset
     end
     while startidx > 0
-        if stringvector ==  view(buffer.value, startidx:endidx)
+        if stringvector == view(buffer.value, startidx:endidx)
             return startidx
         end
         startidx = startidx - 1
@@ -71,6 +89,11 @@ end
 
 Base.length(buffer::StringBuffer) = buffer.size
 capacity(buffer::StringBuffer) = length(buffer.value)
+#reinterpret?
+Base.print(io::IO, buffer::StringBuffer) =
+    unsafe_write(io, pointer(buffer.value), buffer.size)
+Base.print(io::IO, buffer::StringBuffer, range::UnitRange) =
+    unsafe_write(io, pointer(buffer.value) + first(range) - 1, length(range))
 
 function Base.push!(buffer::StringBuffer, char::Char)
     required_size = buffer.size + UInt32(1)
@@ -82,7 +105,13 @@ end
 function Base.append!(buffer::StringBuffer, str::String)
     required_size = buffer.size + UInt32(ncodeunits(str))
     required_size > capacity(buffer) && _resize!(buffer, required_size)
-    unsafe_copyto!(buffer.value, buffer.size + 1, unsafe_wrap(Vector{UInt8}, str), 1, ncodeunits(str))
+    unsafe_copyto!(
+        buffer.value,
+        buffer.size + 1,
+        unsafe_wrap(Vector{UInt8}, str),
+        1,
+        ncodeunits(str),
+    )
     buffer.size = required_size
 end
 
@@ -91,15 +120,34 @@ function Base.insert!(buffer::StringBuffer, str::String, i::Integer)
     required_capacity = buffer.size + ncodeunits(str)
     required_capacity > capacity(buffer) && _resize!(buffer, required_capacity)
     #shift
-    unsafe_copyto!(buffer.value, i + ncodeunits(str), buffer.value, i, buffer.size - i + 1)
+    unsafe_copyto!(
+        buffer.value,
+        i + ncodeunits(str),
+        buffer.value,
+        i,
+        buffer.size - i + 1,
+    )
     #insert
-    unsafe_copyto!(buffer.value, i, unsafe_wrap(Vector{UInt8}, str), 1, ncodeunits(str))
+    unsafe_copyto!(
+        buffer.value,
+        i,
+        unsafe_wrap(Vector{UInt8}, str),
+        1,
+        ncodeunits(str),
+    )
     buffer.size = required_capacity
 end
 
 function Base.delete!(buffer::StringBuffer, range::UnitRange)
-    !_in_bounds(buffer, range) && throw(BoundsError("Bound error size: $size range: $range"))
-    unsafe_copyto!(buffer.value, first(range), buffer.value, last(range) +1 , buffer.size - last(range))
+    !_in_bounds(buffer, range) &&
+        throw(BoundsError("Bound error size: $size range: $range"))
+    unsafe_copyto!(
+        buffer.value,
+        first(range),
+        buffer.value,
+        last(range) + 1,
+        buffer.size - last(range),
+    )
     buffer.size = buffer.size - length(range)
 end
 
@@ -111,23 +159,20 @@ function Base.replace!(buffer::StringBuffer, new_val::String, range::UnitRange)
         buffer[range] = new_val
     elseif offset < 0
         buffer[range] = new_val[1:old_length]
-        insert!(buffer, new_val[old_length+1:new_length], last(range)+1)
+        insert!(buffer, new_val[old_length+1:new_length], last(range) + 1)
     elseif offset > 0
-        buffer[first(range):first(range) + new_length - 1] = new_val
-        delete!(buffer, first(range) + new_length:last(range))
+        buffer[first(range):first(range)+new_length-1] = new_val
+        delete!(buffer, first(range)+new_length:last(range))
     end
 end
-#reinterpret?
-Base.print(io::IO, buffer::StringBuffer) = unsafe_write(io, pointer(buffer.value), buffer.size)
-Base.print(io::IO, buffer::StringBuffer, range::UnitRange) =
-    unsafe_write(io, pointer(buffer.value) + first(range) - 1, length(range))
 
 function _resize!(buffer::StringBuffer, required_size::Integer)
-    calculated_size = Int(floor(buffer.size*RESIZE_COEF))
+    calculated_size = Int(floor(buffer.size * RESIZE_COEF))
     newsize = calculated_size > required_size ? calculated_size : required_size
     resize!(buffer.value, newsize)
 end
 
 _in_bounds(buffer::StringBuffer, i::Integer) = i >= 1 && i <= buffer.size
 
-_in_bounds(buffer::StringBuffer, i::UnitRange) = first(i) >= 1 && last(i) <= buffer.size
+_in_bounds(buffer::StringBuffer, i::UnitRange) =
+    first(i) >= 1 && last(i) <= buffer.size
